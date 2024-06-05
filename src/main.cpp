b17a548c3db95861eb3cc14d7c1074ac4bdb087a
Joshua Thomas <jnthomas522@gmail.com>
2024-06-04 21:13:07 -0400

    Create box and rotate it

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,121 @@
 #include <SDL2/SDL.h>
 #include"screen.h"
 
-int main(int argc, char *argv[]){
-    Screen screen;
+mesh cube;
+Screen screen;
+float fTheta;
 
-    for(int i = 0; i < 100; i++){
-        screen.pixel(rand()%640, rand()%480);
+void drawCube(float fElapsedTime){
+    // Set up rotation matrices
+    mat4x4 matRotZ, matRotX;
+    fTheta += 1.0f * (fElapsedTime/1000);
 
+    // Rotation Z
+    matRotZ.m[0][0] = cosf(fTheta);
+    matRotZ.m[0][1] = sinf(fTheta);
+    matRotZ.m[1][0] = -sinf(fTheta);
+    matRotZ.m[1][1] = cosf(fTheta);
+    matRotZ.m[2][2] = 1;
+    matRotZ.m[3][3] = 1;
+
+    // Rotation X
+    matRotX.m[0][0] = 1;
+    matRotX.m[1][1] = cosf(fTheta * 0.5f);
+    matRotX.m[1][2] = sinf(fTheta * 0.5f);
+    matRotX.m[2][1] = -sinf(fTheta * 0.5f);
+    matRotX.m[2][2] = cosf(fTheta * 0.5f);
+    matRotX.m[3][3] = 1;
+
+    for(auto face : cube.faces){
+        vec3 triProjected[3], triRotatedZ[3], triRotatedX[3];
+
+        MultiplyMatrixVector(cube.vertecis[face.a], triRotatedZ[0], matRotZ);
+        MultiplyMatrixVector(cube.vertecis[face.b], triRotatedZ[1], matRotZ);
+        MultiplyMatrixVector(cube.vertecis[face.c], triRotatedZ[2], matRotZ);
+
+        MultiplyMatrixVector(triRotatedZ[0], triRotatedX[0], matRotX);
+        MultiplyMatrixVector(triRotatedZ[1], triRotatedX[1], matRotX);
+        MultiplyMatrixVector(triRotatedZ[2], triRotatedX[2], matRotX);
+
+        triRotatedX[0].z += 3;
+        triRotatedX[1].z += 3;
+        triRotatedX[2].z += 3;
+
+        MultiplyMatrixVector(triRotatedX[0], triProjected[0], screen.matProj);
+        MultiplyMatrixVector(triRotatedX[1], triProjected[1], screen.matProj);
+        MultiplyMatrixVector(triRotatedX[2], triProjected[2], screen.matProj);
+        
+        
+        // Scale into view
+        triProjected[0].x += 1.0f; triProjected[0].y += 1.0f;
+        triProjected[1].x += 1.0f; triProjected[1].y += 1.0f;
+        triProjected[2].x += 1.0f; triProjected[2].y += 1.0f;
+        triProjected[0].x *= 0.5f * screen.WIDTH;
+        triProjected[0].y *= 0.5f * screen.HEIGHT;
+        triProjected[1].x *= 0.5f * screen.WIDTH;
+        triProjected[1].y *= 0.5f * screen.HEIGHT;
+        triProjected[2].x *= 0.5f * screen.WIDTH;
+        triProjected[2].y *= 0.5f * screen.HEIGHT;
+
+        screen.line(triProjected[0].x, triProjected[0].y,
+                    triProjected[1].x, triProjected[1].y);
+        screen.line(triProjected[1].x, triProjected[1].y,
+                    triProjected[2].x, triProjected[2].y);
+        screen.line(triProjected[2].x, triProjected[2].y,
+                    triProjected[0].x, triProjected[0].y);
     }
+}
+
+int main(int argc, char *argv[]){
+
+    cube.vertecis = {
+        { 0.0f, 0.0f, 0.0f}, //0
+        { 0.0f, 0.0f, 1.0f}, //1
+        { 0.0f, 1.0f, 0.0f}, //2
+        { 0.0f, 1.0f, 1.0f}, //3
+
+        { 1.0f, 0.0f, 0.0f}, //4
+        { 1.0f, 0.0f, 1.0f}, //5
+        { 1.0f, 1.0f, 0.0f}, //6
+        { 1.0f, 1.0f, 1.0f}, //7
+    };
+
+    cube.faces = {
+        //front
+        { 0, 2, 6 },
+        { 0, 6, 4 },
+        
+        //back
+        { 5, 7, 3 },
+        { 5, 6, 1 },
+
+        //bottom
+        { 5, 1, 0 },
+        { 5, 0, 4 },
+
+        //top
+        { 2, 3, 7 },
+        { 2, 7, 6 },
+
+        //right
+        { 4, 6, 7 },
+        { 4, 7, 5 },
+
+        //left
+        { 1, 3, 2 },
+        { 1, 2, 0 },
+    };
+
+    float time1 = SDL_GetTicks64();
 
     while(true){
+        float time2 = SDL_GetTicks64();
+        drawCube(time2 - time1);
+        time1 = time2;
         screen.show();
         screen.input();
+        SDL_Delay(30);
+        screen.clear();
     }
 
     return 0;
--- src/screen.h
+++ src/screen.h
@@ -1,28 +1,57 @@
+#include "util.h"
 #include <SDL2/SDL.h>
 #include <Vector>
+#include <cmath>
 
 class Screen{
     SDL_Event e;
     SDL_Window* window;
     SDL_Renderer* renderer;
     std::vector<SDL_FPoint> points;
+    std::vector<line> lines;
 
     public:
         
-        const int WIDTH = 640;
-        const int HEIGHT = 480;
-        const int SCALE = 1;
+        const int WIDTH = 320;
+        const int HEIGHT = 240;
+        const int SCALE = 2;
+
+        const float fNear = 0.1f;
+        const float fFar = 1000.0f;
+        const float fFov = 90.0f;
+        const float fAspectRatio = ((float) HEIGHT)/((float)WIDTH);
+        const float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+        
+        mat4x4 matProj;
+
         Screen(){
             SDL_Init(SDL_INIT_VIDEO);
             SDL_CreateWindowAndRenderer(
                 WIDTH*SCALE, HEIGHT*SCALE, 0, &window, &renderer
             );
             SDL_RenderSetScale(renderer, SCALE, SCALE);
+
+            matProj.m[0][0] = fAspectRatio * fFovRad;
+            matProj.m[1][1] = fFovRad;
+            matProj.m[2][2] = fFar / (fFar - fNear);
+            matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+            matProj.m[2][3] = 1.0f;
+            matProj.m[3][3] = 0.0f;
+
         }
 
+        void clear(){
+            points.clear();
+            lines.clear();
+        }
+
+
         void pixel(float x, float y){
-            
             points.push_back(SDL_FPoint({x, y}));
+        }
+        
+        void line(float x1, float y1, float x2, float y2){
+            lines.emplace_back(x1, y1, x2, y2);
         }
 
         void show(){
@@ -30,6 +59,11 @@ class Screen{
             SDL_RenderClear(renderer);
 
             SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+            for(auto& line : lines){
+                SDL_RenderDrawLineF(renderer, line.x1, line.y1, line.x2, line.y2);
+            }
+
             for (auto& point: points){
                 SDL_RenderDrawPointF(renderer, point.x, point.y);
             }
